2faa4aab5bef945fd4298c7a666b027b23c0ec73
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-09-12 22:31:19 +0530

    Create Expression program.java
    simple expression java program calculate a area of triangle

---
+++ BasicJava/Expression.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.util.Scanner.*;
+
+class Expression {
+    public static void main(String[] args)
+    {
+        float base,height,area;
+        System.out.println("Enter base and height ");
+
+        Scanner sc = new Scanner(System.in);
+        base = sc.nextFloat();
+        height = sc.nextFloat();
+
+        area = base*height/2;
+        System.out.println("Area of a triangle is:  " + area );
+
+    }
+
+}
+
