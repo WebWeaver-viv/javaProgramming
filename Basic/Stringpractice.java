dc9ead3c2610458e02a188fbb919ef92168e18cd
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-11-13 21:55:41 +0530

    Add Stringpractice class with main method

---
+++ Basic/Stringpractice.java
@@ -0,0 +1,13 @@
+
+public class Stringpractice {
+
+  
+    public static void main(String[] args) {
+        
+        char c[] = {'H','e','l','l','o'};
+          byte b[] = {65,66,67,68};
+         String str1 = new String(c,0,5);
+        System.out.println(str1);
+    }
+    
+}
