2383c255bed82fb31914cd0257a881a3aeca855b
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-11-24 09:04:27 +0530

    Create SCYoung.java

---
+++ Basic/SCYoung.java
@@ -0,0 +1,21 @@
+
+// find a person is  young or not young 
+
+
+package relational;
+import java.util.Scanner;
+public class SCYoung {
+    public static void main(String[] args) {
+       
+        int age;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Age ");
+        age = sc.nextInt();
+        
+        if(age >= 18 ){
+        System.out.println("A person is  Young");
+        } else if ( age < 18){
+        System.out.println("A person is not  Young");
+        }
+    }
+}
