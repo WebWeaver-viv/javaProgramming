01866f2ffd8268e7539e2c57d3694029c46a846d
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-11-24 08:59:49 +0530

    Create SCConditions.java

---
+++ Basic/SCConditions.java
@@ -0,0 +1,21 @@
+// this is  a quetion
+// Find a number odd or even  
+
+
+package relational;
+import java.util.Scanner;
+public class SCConditions {
+    public static void main(String[] args) {
+       
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        n = sc.nextInt();
+        
+        if(n % 2 == 0 ){
+        System.out.println("This number is even");
+        } else if ( n % 2 != 0 ){
+        System.out.println("This number is odd");
+        }
+    }
+}
