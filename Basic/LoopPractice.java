f9d3067df450d4db297f1427e2c0f144d4049319
Jitendra Gupta <jgjitendragupta62@gmail.com>
2025-12-11 10:39:34 +0530

    Add for loop example in LoopPractice.java
    Implemented a for loop to print numbers 1 to 10.

---
+++ Basic/LoopPractice.java
@@ -0,0 +1,40 @@
+
+package looppractice;
+
+
+public class LoopPractice {
+
+    public static void main(String[] args) {
+        
+
+//        int i=1,n=100;
+        
+//        -------------- while loop code ----------------
+
+//        while(i<n)
+//        {
+//            System.out.println(i);
+//            i=i*2;
+//        }
+//        System.out.println("BYE");
+
+//        ------------- do while loop code --------------
+
+//         do
+//        {
+//            System.out.println(i);
+//            i=i*2;
+//        }while(i<n);
+//        System.out.println("BYE");
+
+//         --------------for loop code --------------
+
+            for(int i = 1; i<=10;i++)
+            {
+                System.out.println(i);
+            }
+            System.out.println("BYE");
+
+    }
+    
+}
